41ead4a1745a37eb51fdc8e96e7fb365b15ff64b
CarrumDraws <calumchansy@gmail.com>
2022-09-25 00:07:42 -0700

    added darkmode

--- techtests/src/Tests/AddiGlobTwo/context/PromoContext.js
+++ techtests/src/Tests/AddiGlobTwo/context/PromoContext.js
@@ -11,11 +11,25 @@ export function PromoProvider({ children }) {
     if (theme) {
       // true = light
       console.log("Light");
+      rootstyle.setProperty("--primary", "rgb(69, 160, 209)");
+      rootstyle.setProperty("--primary-fade", "rgb(69, 160, 209, 0)");
+      rootstyle.setProperty("--primary-text", "rgb(255, 255, 255)");
+
+      rootstyle.setProperty("--secondary", "rgb(8, 54, 64)");
+
       rootstyle.setProperty("--body", "rgb(69, 209, 202)");
+      rootstyle.setProperty("--body-card", "rgb(188, 240, 237)");
     } else {
       // false = dark
       console.log("Dark");
-      rootstyle.setProperty("--body", "rgb(8, 54, 64)");
+      rootstyle.setProperty("--primary", "rgb(8, 54, 64)");
+      rootstyle.setProperty("--primary-fade", "rgb(8, 54, 64, 0)");
+      rootstyle.setProperty("--primary-text", "rgb(69, 160, 209)");
+
+      rootstyle.setProperty("--secondary", "rgb(69, 209, 202)");
+
+      rootstyle.setProperty("--body", "rgb(8, 27, 32)");
+      rootstyle.setProperty("--body-card", "rgb(8, 54, 64)");
     }
   }, [theme]);
 
